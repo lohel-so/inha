7e6e75fc4be667aa32b1f2e3239c37e0b3843bde
lohel-so <sohyeonkim@inha.edu>
2024-10-13 16:58:20 +0900

    init

---
+++ web02/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log("----")
+  res.send('respond with a resourcde');
+});
+
+
+module.exports = router;
