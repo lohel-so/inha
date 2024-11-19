ef372d2e428d68a7b9a102feb378254b86527209
lohel-so <sohyeonkim@inha.edu>
2024-11-19 21:58:39 +0900

    web11

---
+++ web11/routes/users.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* 로그인 */
+router.get('/login', function(req, res, next) {
+  res.render('index', {title: '로그인', pageName:'users/login.ejs'});  
+});
+
+/* 회원가입 */
+router.get('/join', function(req, res, next) {
+  res.render('index', {title: '회원가입', pageName:'users/join.ejs'});  
+});
+
+/* 장바구니 */
+router.get('/cart', function(req, res, next) {
+  res.render('index', {title: '장바구니', pageName:'users/cart.ejs'});  
+});
+
+/* 마이페이지 */
+router.get('/mypage', function(req, res, next) {
+  res.render('index', {title: '마이페이지', pageName:'users/mypage.ejs'});  
+});
+
+
+module.exports = router;
